b3b0477f8be3c3002c9a3bfb8d9fba666c235053
shalomdorgeles0101-arch <shalomdorgeles0101@gmail.com>
2026-08-20 00:56:10 +0000

    modification

---
+++ prestige/style.js
@@ -0,0 +1,6 @@
+  const menu = document.querySelector('.menu')
+        const btn = document.querySelector('.btn-menu')
+
+        btn.addEventListener('click', () => {
+        menu.classList.toggle('active')
+    })
